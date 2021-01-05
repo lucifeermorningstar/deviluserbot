4c44ced158c74dd9d73960bb0b1cdbbf6924079a
lucifeermorningstar <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-05 11:21:48 +0530

    Create string_session.py

---
+++ string_session.py
@@ -0,0 +1,32 @@
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
+print("")
+print("""Welcome To DevilUserBot String Generator By Devil""")
+print("""Kindly Enter Your Details To Continue ! """)
+
+API_KEY = input("API_KEY: ")
+API_HASH = input("API_HASH: ")
+
+while True:
+    try:
+        with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+            print("String Sent To Your Saved Message, Store It To A Safe Place!! ")
+            print("")
+            session = client.session.save()
+            client.send_message(
+                "me",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @Deviluserbot For Any Help !",
+            )
+
+            print(
+                "Thanks for Choosing DevilUserBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+            )
+    except:
+        print("")
+        print(
+            "Wrong phone number \n make sure its with correct country code. Example : +919961998999 ! Kindly Retry"
+        )
+        print("")
+        continue
+    break
