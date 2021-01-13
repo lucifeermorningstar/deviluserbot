bcd78ac1daefa826fd19d8ef630b06d226d2fd2e
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-13 16:43:55 +0530

    Update git.py

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -26,9 +26,9 @@ async def _(event):
 
         animation_chars = [
 
-            "https://github.com/HellBoy-OP/HellBot",
+            "https://github.com/lucifeermorningstar/deviluserbot",
 
-            "https://github.com/HellBoy-OP/HellBot"
+            "https://github.com/lucifeermorningstar/deviluserbot"
         ]
 
         for i in animation_ttl:
