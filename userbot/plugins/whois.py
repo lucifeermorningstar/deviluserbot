adc3dae3bd6f2a4378736d765862e031fb397feb
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-25 10:27:19 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -125,7 +125,7 @@ async def fetch_info(replied_user, event):
     )
     username = "@{}".format(username) if username else ("This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
-    caption = "<b>USER INFO from Hêllẞø†'s database :</b>\n\n"
+    caption = "<b>USER INFO from 𝐃𝐞𝐯𝐢𝐥𝐔𝐬𝐞𝐫𝐁𝐨𝐭's database :</b>\n\n"
     caption += f"👤 First Name: {first_name} {last_name}\n"
     caption += f"🤵 Username: {username}\n"
     caption += f"🔖 ID: <code>{user_id}</code>\n"
